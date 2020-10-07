4923e085e77085d1c15174faca503862d0eab340
Fernando Garcia <fernandogdac@gmail.com>
2020-10-07 11:30:10 -0300

    Comentando o codigo

--- php/arvore.class.php
+++ php/arvore.class.php
@@ -4,29 +4,36 @@ class Arvore {
 
     public function inserir($node, $valor) {
 
+        // Verifica se a árvore já foi criada
         if ($node != NULL) {
 
+            // Verifica se o valor a ser inserido é menor que o do nodo corrente da árvore, se sim vai para a subarvore esquerda
             if ($valor < $node->valor) {
 
+                // Se tiver elemento no modo esquerdo continua a busca
                 if ($node->esquerda != NULL) {
 
                     $this->inserir($node->esquerda, $valor);
 
                 } else {
 
+                    // se nodo esquerdo vazio insere o novo modo aqui
                     echo "Inserido" . $valor . " a esquerda de " . $node->valor . "<br>";
                     $node->esquerda = new No($valor);
                     $GLOBALS["valorEsquerda"] += 200;
                 }
 
+                // Verifica se o valor a ser inserido é maior que o nodo corrente da árvore, se sim vai para subarvore direta
             } elseif ($valor > $node->valor) {
 
+                // Se tiver elemento no modo direto continua a busca
                 if ($node->direita != NULL) {
 
                     $this->inserir($node->direita, $valor);
 
                 } else {
 
+                    // Se nodo direito vazio insere o novo modo aqui
                     echo "Inserindo " . $valor . " a direita de " . $node->valor . "<br>";
                     $node->direita = new No($valor);
                     $GLOBALS["valorDireita"] += 100;
